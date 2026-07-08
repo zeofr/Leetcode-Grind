d8ffcbe01daa69ab77d7bb233e7aa6be01ef8c69
Zaid Ahmad <157026468+zeofr@users.noreply.github.com>
2026-07-08 10:17:52 +0530

    Add solution for 0197. Rising Temperature

---
+++ sql-databases/0197-rising-temperature/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT w1.id 
+FROM Weather w1
+JOIN Weather w2
+    ON DATE_SUB(w1.recordDate, INTERVAL 1 DAY) = w2.recordDate
+WHERE 
+    w1.temperature > w2.temperature
