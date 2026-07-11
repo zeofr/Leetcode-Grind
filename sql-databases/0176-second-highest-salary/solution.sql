2c7d8c36c54302d4c232923c30f8059176f0bbd6
Zaid Ahmad <157026468+zeofr@users.noreply.github.com>
2026-07-11 08:57:01 +0530

    Add solution for 0176. Second Highest Salary

--- sql-databases/0176-second-highest-salary/solution.sql
+++ sql-databases/0176-second-highest-salary/solution.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
 SELECT( 
+    SELECT salary
     FROM (
-        SELECT *,
+        SELECT DISTINCT salary,
             DENSE_RANK() OVER (
                 ORDER BY salary DESC
             ) as rnk
         FROM Employee
     ) x
     WHERE rnk = 2
-    SELECT salary
 ) as SecondHighestSalary
