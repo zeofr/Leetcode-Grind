7de07fd3846f48e08b60f8b098c45b3bf4b66a76
Zaid Ahmad <157026468+zeofr@users.noreply.github.com>
2026-07-17 18:13:03 +0530

    Add solution for 0042. Trapping Rain Water

---
+++ dsa/0042-trapping-rain-water/solution.java
@@ -0,0 +1,28 @@
+                    res += maxLeft - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    res += maxRight - height[right];
+                } else {
+                    maxLeft = height[left];
+                if (height[left] >= maxLeft) {
+            if (height[left] <= height[right]){
+        while (left <= right) {
+
+        int maxLeft = 0, maxRight = 0;
+        int res = 0;
+                }
+                right--;
+            }
+        }
+        return res;
+    }
+}
+
+        int left = 0, right = n - 1;
+        int n = height.length;
+    public int trap(int[] height) {
+class Solution {
