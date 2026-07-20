65d3c052e0f72c05630f575eae687fd938d632d1
Zaid Ahmad <157026468+zeofr@users.noreply.github.com>
2026-07-20 22:38:43 +0530

    Add solution for 0198. House Robber

---
+++ dsa/0198-house-robber/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        
+        int dp[] = new int[n];
+        dp[0] = nums[0];
+        for(int i = 2; i < n; i++){
+            dp[i] = Math.max( nums[i] + dp[i-2], dp[i-1] );
+        }
+    }
+        dp[1] = Math.max(nums[0], nums[1]);
+        int n = nums.length;
+
+
+        return dp[n-1];
+        if(n == 1) return nums[0];
+
+}
