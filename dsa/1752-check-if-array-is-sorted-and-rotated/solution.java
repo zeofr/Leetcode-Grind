dbd585d08940e25d96dae01c04ef96f31c32bb86
Zaid Ahmad <157026468+zeofr@users.noreply.github.com>
2026-07-26 08:12:37 +0530

    Add solution for 1752. Check if Array Is Sorted and Rotated

---
+++ dsa/1752-check-if-array-is-sorted-and-rotated/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int i,n=nums.length;
+        for(i = 0 ; i < n-1 ; i++){
+            if(nums[i]>nums[i+1]) break;
+        }
+
+        if(i==n-1) return true;
+
+        for(int j = i+1 ; j< n-1 ; j++){
+            if(nums[j]>nums[j+1]) return false;
+        }
+
+        if (nums[n - 1] > nums[0]) {
+            return false;
+        }
+
+        return true;
+
+    }
+}
