62784527e4c3cde0704addb57aa22cac6c84d175
Zaid Ahmad <157026468+zeofr@users.noreply.github.com>
2026-08-03 12:05:19 +0530

    Add solution for 0073. Set Matrix Zeroes

---
+++ dsa/0073-set-matrix-zeroes/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length ;
+        int row[] = new int[m], col[] = new int[n] ;
+
+        for(int i = 0; i<m ; i++){
+            for(int j = 0; j<n ; j++){
+                if(matrix[i][j]==0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for(int i = 0; i<m ; i++){
+            for(int j = 0; j<n; j++){
+                if(row[i] == 1 || col[j] == 1)
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
