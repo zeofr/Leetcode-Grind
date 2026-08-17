4bce055df372c2b9acb748e13f3707662f0cd349
Zaid Ahmad <157026468+zeofr@users.noreply.github.com>
2026-08-17 09:26:09 +0530

    Add solution for 0054.

--- dsa/0054-spiral-matrix/solution.java
+++ dsa/0054-spiral-matrix/solution.java
@@ -1,11 +1,15 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
+    public List<Integer> spiralOrder(int[][] 
+    matrix) {
+        List<Integer> result = new 
+        ArrayList<>();
         int m = matrix.length;
         int n = matrix[0].length;
-        int top = 0 , left = 0 , bottom = m-1, right = n-1;
+        int top = 0 , left = 0 , bottom = 
+        m-1, right = n-1;
 
-        while(top <= bottom && left <= right){
+        while(top <= bottom && left <= right)
+        {
             for(int i = left; i<=right; i++)
                 result.add(matrix[top][i]);
             top++;
@@ -13,14 +17,13 @@ class Solution {
                 result.add(matrix[i][right]);
             right--;
             if (top <= bottom)
-            for(int i = right; i >= left; i--)
-                result.add(matrix[bottom][i]);
+            for(int i = right; i >= left; 
+            i--)
+                result.add(matrix[bottom]
+                [i]);
             bottom--;
             if (left <= right)
             for(int i= bottom; i>=top; i--)
                 result.add(matrix[i][left]);
             left++;
         }
-        return result;
-    }
-}
