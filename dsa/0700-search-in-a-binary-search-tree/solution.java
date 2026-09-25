451b7048684f3be444ec4db62d1565f320b83305
Zaid Ahmad <157026468+zeofr@users.noreply.github.com>
2026-09-25 06:34:34 +0530

    Add solution for 0700. Search in a Binary Search Tree

---
+++ dsa/0700-search-in-a-binary-search-tree/solution.java
@@ -0,0 +1,26 @@
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null) {
+            if (root.val == val)
+                return root;
+            if (val < root.val)
+                root = root.left;
+            else
+                root = root.right;
+        }
+        return null;
+    }
+}
+ *     int val;
+ * public class TreeNode {
